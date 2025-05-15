496a35a50087c828a93e89f748a3d3b8762bb532
Alberto Lazari <alberto.lazari@gmail.com>
2025-05-15 12:28:43 +0200

    Character moves

---
+++ Source/TowerHop/TowerHop.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TowerHop : ModuleRules
+{
+	public TowerHop(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/TowerHop/TowerHopGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerHopGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATowerHopGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATowerHopGameMode();
+};
+
+
+
